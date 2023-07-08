2e5f04cfd85568607f95363447c5955c12f7e469
Subhash Barela <barelasubhash16@gmail.com>
2023-07-08 09:38:10 +0530

    [subhash] insert data

--- CollectionProgrammingPractice/Program.cs
+++ CollectionProgrammingPractice/Program.cs
@@ -10,13 +10,37 @@ namespace CollectionProgrammingPractice
     {
         static void Main(string[] args)
         {
-
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-
-            list.display();
+            Console.WriteLine("1:Create LinkedList \n2: Add Data");
+            Console.WriteLine("Enter the choice");
+            int choice=int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70); 
+                        list.display();
+                        break;
+                    }
+                    case 2:
+                    {
+                        Console.WriteLine("insert the first element into linked list :");
+                        int first = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("insert the second element into linked list :");
+                        int second = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("insert the third element into linked list :");
+                        int third = Convert.ToInt32(Console.ReadLine());
+                        list.Add(first);
+                        list.Add(second);
+                        list.Add(third);
+                        list.display();
+                        break;
+                    }
+           
+        }
+           
             Console.ReadLine();
         }
     }
